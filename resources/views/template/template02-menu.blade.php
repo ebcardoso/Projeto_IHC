ea2c8aacfa6d5eb0092907e8bacc0cd91da313b7
Breno Cardoso <elisiobreno@gmail.com>
2019-07-13 05:24:17 -0300

    Logout no Menu lateral

--- resources/views/template/template02-menu.blade.php
+++ resources/views/template/template02-menu.blade.php
@@ -16,29 +16,6 @@
             </div>
             <!-- /.navbar-header -->
 
-            <ul class="nav navbar-top-links navbar-right">
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        {{ Auth::user()->name }} 
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="{{route('perfil')}}"><i class="fa fa-user fa-fw"></i> Meu Perfil </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="{{ route('logout') }}">
-                                <i class="fa fa-sign-out fa-fw"></i> Logout
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
@@ -59,6 +36,9 @@
                         </li>
                         <li>
                             <a href="{{route('exercicios.inicial')}}"><i class="fa fa-tag fa-fw"></i>Exercícios</a>
+                        </li>
+                        <li>
+                            <a href="{{route('logout')}}"><i class="fa fa-tag fa-fw"></i>Sair</a>
                         </li>
                     </ul>
                 </div>
